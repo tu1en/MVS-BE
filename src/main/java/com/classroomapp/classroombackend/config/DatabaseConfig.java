702a8101b729e49b34e48022129c713b28b74595
turmatoes <nguyenxuanphilong@gmail.com>
2025-06-18 20:50:11 +0700

    reconfig db

--- src/main/java/com/classroomapp/classroombackend/config/DatabaseConfig.java
+++ src/main/java/com/classroomapp/classroombackend/config/DatabaseConfig.java
@@ -17,7 +17,7 @@ public class DatabaseConfig {
               .driverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver")
               .url("jdbc:sqlserver://localhost:1433;databaseName=SchoolManagementDB;encrypt=true;trustServerCertificate=true;characterEncoding=UTF-8;useUnicode=true;")
                 .username("sa")
-                .password("123456")
+                .password("Hoangduc02@")
                 .build();
     }
 }
