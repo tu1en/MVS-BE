f5268f2e8dce2ed1c787c51713a5fb5c85024661
ClassroomApp Developer <classroomapp@example.com>
2025-06-25 13:07:25 +0700

    Giải quyết xung đột merge và sửa lỗi import trong DatabaseConfig.java

--- src/main/java/com/classroomapp/classroombackend/config/DatabaseConfig.java
+++ src/main/java/com/classroomapp/classroombackend/config/DatabaseConfig.java
@@ -1,3 +1,13 @@
+package com.classroomapp.classroombackend.config; 
+ 
+import org.springframework.boot.autoconfigure.domain.EntityScan; 
+import org.springframework.context.annotation.Bean; 
+import org.springframework.context.annotation.ComponentScan; 
+import org.springframework.context.annotation.Configuration; 
+import org.springframework.context.annotation.Primary; 
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories; 
+import javax.sql.DataSource; 
+import com.zaxxer.hikari.HikariDataSource; 
 package com.classroomapp.classroombackend.config;
 
 import javax.sql.DataSource;
