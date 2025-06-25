55577f33889e4532db297c9b95e764152be12062
ClassroomApp Developer <classroomapp@example.com>
2025-06-25 14:37:40 +0700

    tiếp tục sửa lỗi

--- src/main/java/com/classroomapp/classroombackend/config/DatabaseConfig.java
+++ src/main/java/com/classroomapp/classroombackend/config/DatabaseConfig.java
@@ -1,15 +1,5 @@
 package com.classroomapp.classroombackend.config; 
  
-import org.springframework.boot.autoconfigure.domain.EntityScan; 
-import org.springframework.context.annotation.Bean; 
-import org.springframework.context.annotation.ComponentScan; 
-import org.springframework.context.annotation.Configuration; 
-import org.springframework.context.annotation.Primary; 
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories; 
-import javax.sql.DataSource; 
-import com.zaxxer.hikari.HikariDataSource; 
-package com.classroomapp.classroombackend.config;
-
 import javax.sql.DataSource;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -55,7 +45,7 @@ public class DatabaseConfig {
         dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         dataSource.setJdbcUrl("jdbc:sqlserver://localhost:1433;databaseName=SchoolManagementDB;encrypt=true;trustServerCertificate=true;characterEncoding=UTF-8;useUnicode=true;sendTimeAsDateTime=false;");
         dataSource.setUsername("sa");
-        dataSource.setPassword("Hoangduc02@");
+        dataSource.setPassword("123456");
         dataSource.setAutoCommit(false);
         dataSource.setTransactionIsolation("TRANSACTION_READ_COMMITTED");
         dataSource.setMaximumPoolSize(10);
