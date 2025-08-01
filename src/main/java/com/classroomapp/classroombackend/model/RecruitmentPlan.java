9144fbe3dfb1182d436b304ca166b8143449759f
turmatoes <nguyenxuanphilong@gmail.com>
2025-08-01 11:06:30 +0700

    status bug + unicode bug

--- src/main/java/com/classroomapp/classroombackend/model/RecruitmentPlan.java
+++ src/main/java/com/classroomapp/classroombackend/model/RecruitmentPlan.java
@@ -11,7 +11,7 @@ public class RecruitmentPlan {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = "NVARCHAR(500)")
     private String title;
 
     @Column(nullable = false)
