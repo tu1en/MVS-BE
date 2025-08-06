41a5c561715aae5a28061f3cce90b37b28bb548e
turmatoes <nguyenxuanphilong@gmail.com>
2025-08-06 12:32:52 +0700

    minor fix

--- src/main/java/com/classroomapp/classroombackend/repository/RecruitmentApplicationRepository.java
+++ src/main/java/com/classroomapp/classroombackend/repository/RecruitmentApplicationRepository.java
@@ -10,10 +10,10 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RecruitmentApplicationRepository extends JpaRepository<RecruitmentApplication, Long> {
     @Modifying
-    @Query("DELETE FROM RecruitmentApplication ra WHERE ra.jobPositionId = :jobPositionId")
+    @Query("DELETE FROM RecruitmentApplication ra WHERE ra.jobPosition.id = :jobPositionId")
     void deleteByJobPositionId(@Param("jobPositionId") Long jobPositionId);
     
     @Modifying
-    @Query("DELETE FROM RecruitmentApplication ra WHERE ra.jobPositionId IN (SELECT jp.id FROM JobPosition jp WHERE jp.recruitmentPlan.id = :recruitmentPlanId)")
+    @Query("DELETE FROM RecruitmentApplication ra WHERE ra.jobPosition.id IN (SELECT jp.id FROM JobPosition jp WHERE jp.recruitmentPlan.id = :recruitmentPlanId)")
     void deleteByJobPosition_RecruitmentPlanId(@Param("recruitmentPlanId") Long recruitmentPlanId);
 } 
