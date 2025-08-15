0f739494dc83b53e1b6efba35b0e715ec211fbfe
tu1en <thinhcche171897@fpt.edu.vn>
2025-08-15 09:13:18 +0700

    dbconfig null

---
+++ src/main/java/com/classroomapp/classroombackend/config/DatabaseConfig.java
@@ -0,0 +1,32 @@
+package com.classroomapp.classroombackend.config; 
+ 
+import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.classroomapp.classroombackend.repository")
+@EntityScan(basePackages = "com.classroomapp.classroombackend.model")
+public class DatabaseConfig {
+
+    @Bean
+    @Primary
+    public DataSource dataSource() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        dataSource.setJdbcUrl("jdbc:sqlserver://localhost:1433;databaseName=SchoolManagementDB;encrypt=false;trustServerCertificate=true;sendStringParametersAsUnicode=true;sendTimeAsDateTime=false;");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("123456");
+        // dataSource.setAutoCommit(false); // This can prevent seeders from committing data. Let Spring's @Transactional manage commits.
+        dataSource.setTransactionIsolation("TRANSACTION_READ_COMMITTED");
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setMinimumIdle(5);
+        return dataSource;
+    }
+}
